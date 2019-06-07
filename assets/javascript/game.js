b6324f980c814e89dcb03d3bd08842b58b1a67ff
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-06-06 23:47:13 -0500

    added array of all the words to be used

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+var words = [
+    { w: "albus dumbledore" }
+    { w: "baby nifflers" }
+    { w: "pickett the bowtruckle" }
+    { w: "bunty" }
+    { w: "chupacabra" }
+    { w: "credence barebone" }
+    { w: "demiguise" }
+    { w: "gellert grindelwald" }
+    { w: "jacob kowalski" }
+    { w: "leta lestrange" }
+    { w: "matagot" }
+    { w: "nagini" }
+    { w: "newt scamander" }
+    { w: "nicolas flamel" }
+    { w: "niffler" }
+    { w: "obscurus" }
+    { w: "occamy" }
+    { w: "persimmon" }
+    { w: "pistachio" }
+    { w: "porpentina goldstein" }
+    { w: "pumpernickel" }
+    { w: "pumpkin" }
+    { w: "queenie goldstein" }
+    { w: "swooping evil" }
+    { w: "theseus scamander" }
+    { w: "thunderbird" }
+
+]
