bc929f446ee3531f7133c2ceafd4f3210daba18c
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-06-08 18:49:38 -0500

    variable split defined to split up the currentWord

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,15 @@
+// list of words
+
 var words = ["albus dumbledore", "baby nifflers", "pickett the bowtruckle", "bunty", "chupacabra", "credence barebone", "demiguise", "gellert grindelwald", "jacob kowalski", "leta lestrange", "matagot", "nagini", "newt scamander", "nicolas flamel", "niffler", "obscurus", "occamy", "persimmon", "pistachio", "porpentina goldstein", "pumpernickel", "pumpkin", "queenie goldstein", "swooping evil", "theseus scamander", "thunderbird"]
 
-for(var i=0; i < words.length; i++){
+// for(var i=0; i < words.length; i++){
     
-}
+// }
 
-var currentWord = words[Math.floor(Math.random() * words.length)]
 
-console.log(currentWord)
+// grabbing a word
+var currentWord = words[Math.floor(Math.random() * words.length)];
+
+var split = currentWord.split('');
+
+console.log(split);
